9f56a70dc1602a15b87dc318989ae259db49a904
Benjamin <benjaminho@berkeley.edu>
2017-09-27 17:34:49 -0700

    hw2 submission

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,5 +1,13 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
+  attr_accessor :baz
+
   def initialize(baz)
+      @baz = baz
   end
+  
+  def bar(first, hash)
+      return first.to_s + @baz.to_s + hash[:sat].to_s
+  end
+  
 end
